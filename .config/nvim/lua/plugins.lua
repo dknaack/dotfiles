283cfbdef68da057b7c6ee14fa88e91a61ac3129
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-06 20:04:17 +0200

    added treesitter plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,4 +3,5 @@ local packer = require 'packer'
 return packer.startup(function()
     use 'wbthomason/packer.nvim'
     use 'tpope/vim-sensible'
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
