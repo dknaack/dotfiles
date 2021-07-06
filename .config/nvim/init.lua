60714ec41d6b16e1f1fbc80e0fa478c70df395ce
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-06 19:35:26 +0200

    added nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,27 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local o = vim.o
+local bo = vim.bo
+local wo = vim.wo
+
+-- options
+o.title = true
+o.showmode = false
+o.ruler = false
+o.mouse = 'a'
+o.clipboard = 'unnamedplus'
+o.encoding = 'utf-8'
+o.wildmode = 'longest,list,full'
+
+o.ignorecase = true
+o.ignorecase = true
+o.smartcase = true
+o.incsearch = true
+
+o.splitbelow = true
+o.splitright = true
+
+-- window options
+wo.number = true
+wo.relativenumber = true
