33dc97f57c7523c09cb7b00b7e126144c994e6f2
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-06 20:37:15 +0200

    added vim-fugitive

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,5 +4,6 @@ return packer.startup(function()
     use 'wbthomason/packer.nvim'
     use 'tpope/vim-sensible'
     use 'tpope/vim-surround'
+    use 'tpope/vim-fugitive'
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
