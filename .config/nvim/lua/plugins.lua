fd2fa922a4c952ee208b60954193dd7cd6f9d98f
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-06 20:07:25 +0200

    added vim-surround plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,5 +3,6 @@ local packer = require 'packer'
 return packer.startup(function()
     use 'wbthomason/packer.nvim'
     use 'tpope/vim-sensible'
+    use 'tpope/vim-surround'
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
