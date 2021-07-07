b401ca57fa759a28eb59b8379f57b475e3996c54
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-07 11:54:29 +0200

    fixed starting jdtls

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,8 @@
+local M = {}
 local install = require 'lspinstall'
 local config = require 'lspconfig'
 local jdtls = require 'jdtls'
+local util = require 'util'
 
 install.setup()
 local servers = install.installed_servers()
@@ -8,4 +10,15 @@ for _, server in pairs(servers) do
     config[server].setup {}
 end
 
-jdtls.start_or_attach { cmd = { 'java-lsp.sh' } }
+function M.setup_java()
+    jdtls.start_or_attach { 
+        cmd = { 'java-lsp.sh' },
+        root_dir = jdtls.setup.find_root({ 'pom.xml' })
+    }
+end
+
+util.create_augroup({
+    { 'FileType', 'java', 'lua require("lsp").setup_java()' }
+}, 'jdtls')
+
+return M
