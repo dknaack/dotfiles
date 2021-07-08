7bb8db1c363a39711531450eea7bd18db2983b30
Daniel Knaack <daniel.knaack@protonmail.com>
2021-07-08 10:33:03 +0200

    removed duplicate functions

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -71,16 +71,6 @@ return require('packer').startup(function(use)
 
             local check_back_space = function()
                 local col = vim.fn.col('.') - 1
-
-                return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-            end
-
-            local t = function(str)
-                return vim.api.nvim_replace_termcodes(str, true, true, true)
-            end
-
-            local check_back_space = function()
-                local col = vim.fn.col('.') - 1
                 return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
             end
 
